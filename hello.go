d5d4dfe3e0957a358a926b34ef89236d694c9f2d
zetagamma <dpenillac@correo.usbcali.edu.co>
2021-06-25 03:35:31 -0500

    prolvl tui prob1

--- hello.go
+++ hello.go
@@ -1,9 +1,119 @@
 package main
 
-import "fmt"
+// bubbletea to-do-list probando
+
+import (
+	"fmt"
+	"os"
+
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+// Vamos a hacer una lista.
+	// Ahora el modelo:
+
+type model struct {
+	choices []string 		  // items de la lista
+	cursor int				  // para moverse entre los items
+	selected map[int]struct{} // cuales items estan seleccionados
+}
+
+// init, update, view
+
+var initialModel = model{
+	// La lista es una lista de productos:
+	choices: []string{"yerbini suave","birritas frias","pizzitas melas","programar TUIs"},
+
+	// Un mapa indica bien cuales estan seleccionadas.
+	selected: make(map[int]struct{}),
+}
+
+func (m model) Init() tea.Cmd{
+	// return `nil` -> no + I/O
+	return nil 
+}
+
+// Update (tea.KeyMsg)
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	switch msg := msg.(type){
+		// Hay una tecla presionada:
+		case tea.KeyMsg:
+
+			// Cual?
+			switch msg.String() {
+
+				//Salir del programa
+			case "ctrl+c", "q":
+				return m, tea.Quit
+
+			case "up", "k":
+				if m.cursor > 0 {
+					m.cursor--
+				}
+
+			case "down","j":
+				if m.cursor < len(m.choices)-1 {
+					m.cursor++
+				}
+			
+			// enter, spacebar: select item
+			case "enter", " ":
+				_, ok := m.selected[m.cursor]
+				if ok {
+					delete(m.selected,m.cursor)
+				} else{
+					m.selected[m.cursor] = struct{}{}
+				}
+			}
+	}
+
+	// return updated model, no command
+	return m,nil
+}
+
+// View
+
+func (m model) View() string {
+	// header
+	s := "Melo pa una fiestica, tener:\n\n"
+
+	//iterar sobre choices
+	for i,choice := range m.choices {
+		// cursor en lugar i
+		cursor := " " // no
+		if m.cursor == i {
+			cursor = ">" // cursor
+		}
+
+		//cursor act
+		checked := " " // no act
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // act, seleccionado
+		}
+
+		// Renderizar fila:
+		s += fmt.Sprintf("%s [%s] %s\n",cursor, checked, choice)
+		
+	}
+	
+	// footer:
+	s += "\n Presionar q para salir.\n"
+
+	return s
+}
+
+
 
 func main() {
-	fmt.Println("Hello,World!)
+	//fmt.Println("Hello,World!")
+
+	p := tea.NewProgram(initialModel)
+	if err := p.Start(); err != nil {
+		fmt.Printf("Error pana! %v", err)
+		os.Exit(1)
+	}
 }
+
+
 
 
