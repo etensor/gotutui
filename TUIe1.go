a5b52254735a20c56e760da4100781314474b57c
zetagamma <dpenillac@correo.usbcali.edu.co>
2021-07-13 01:43:26 -0500

    hmm

--- TUIe1.go
+++ TUIe1.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 
+	. "github.com/achannarasappa/term-grid"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/etensor/gotutui/countdown"
 )
@@ -12,12 +13,36 @@ var style = lipgloss.NewStyle().
 	Foreground(lipgloss.Color("#101D07")).
 	Background(lipgloss.Color("#ADC0D8")).
 	PaddingTop(2).
-	PaddingLeft(4).
-	Width(22)
+	PaddingLeft(4)
+
+var stilacho = lipgloss.NewStyle().
+	Italic(true).
+	Foreground(lipgloss.Color("#FAFAFA")).
+	Background(lipgloss.Color("#101D07")).
+	PaddingTop(2).
+	PaddingLeft(4)
 
 func main() {
+
+	out := Render(
+		Grid{
+			GutterVertical:   2,
+			GutterHorizontal: 3,
+			Rows: []Row{
+				{
+					Width: 50,
+					Cells: []Cell{
+						{Width: 10, Text: stilacho.Render("Cuenti"), Overflow: WrapWord},
+						{Width: 20, Text: style.Render("Chimba")},
+					},
+				},
+			},
+		})
+
+	fmt.Print(out)
+	//fmt.Println(stilacho.Render("Cuenti"))
 	fmt.Println(style.Render("Hello there."))
+	countdown.Alive()
 	mainss()
 	countdown.Countdown()
-	countdown.Alive()
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/etensor/gotutui
 go 1.16
 
 require (
+	github.com/achannarasappa/term-grid v0.2.4 // indirect
 	github.com/charmbracelet/bubbletea v0.14.1
 	github.com/charmbracelet/lipgloss v0.3.0
 )
